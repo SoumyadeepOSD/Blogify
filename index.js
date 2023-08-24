ecb64d2e01580ed2706c0901d23d597a0277a428
SoumyadeepOSD <opencontainer255@gmail.com>
2023-08-24 08:55:35 +0530

    Blog stored in MongoDB, successfully rendered data in Homepage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
+const Blog = require('./models/blog');
+
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
 
@@ -20,9 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
+    const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
